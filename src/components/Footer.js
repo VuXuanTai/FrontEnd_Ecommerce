3c9cec05b4c17de62332ea8f23323729264a2f6b
VuXuanTai <taivu.xd.k64@gmail.com>
2023-10-13 10:59:27 +0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,102 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import {BsLinkedin, BsGithub, BsYoutube, BsInstagram} from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+                <img src="images/newsletter.png" alt="newsletter" />
+                <h2 className="mb-0 text-white">Sign Up For Newsletter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-1"
+                  placeholder="Your email address"
+                  aria-label="Your email address"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
 
-export default Footer
+
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">ADD: 245 Định Công, Hoàng Mai, Hà Nội</address>
+                <a href="tel:+84 978172115" className="mt-3 d-block mb-1 text-white">Tel: +84 978172115</a>
+                <a href="mailto:taivx.1312@gmail.com" className="mt-2 d-block mb-0 text-white">
+                  taivx.1312@gmail.com
+                </a>
+                <div className="social_icons d-flex align-items-center gap-30 mt-4">
+                  <a className="text-white" href="#"><BsLinkedin className="fs-4"/></a>
+                  <a className="text-white" href="#"><BsInstagram className="fs-4"/></a>
+                  <a className="text-white" href="#"><BsGithub className="fs-4"/></a>
+                  <a className="text-white" href="#"><BsYoutube className="fs-4"/></a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-3 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-3 mb-1">Refund Policy</Link>
+                <Link className="text-white py-3 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-3 mb-1">Terms & Conditions</Link>
+                <Link className="text-white py-3 mb-1">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-3 mb-1">About Us</Link>
+                <Link className="text-white py-3 mb-1">Faq</Link>
+                <Link className="text-white py-3 mb-1">Contact</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-3 mb-1">Laptops</Link>
+                <Link className="text-white py-3 mb-1">Headphones</Link>
+                <Link className="text-white py-3 mb-1">Tablet</Link>
+                <Link className="text-white py-3 mb-1">Watch</Link>
+              </div>
+            </div>
+           </div> 
+        </div>
+      </footer>
+
+
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()}; Powered by Tai Vu
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import {BsSearch} from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
 
 const Header = () => {
   return (
@@ -51,29 +51,87 @@ const Header = () => {
                 <div>
                   <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="/images/compare.svg" alt="compare"></img>
-                    <p>Compare <br/> Products</p>
+                    <p className="mb-0">
+                      Compare <br /> Products
+                    </p>
                   </Link>
                 </div>
                 <div>
                   <Link className="d-flex align-items-center gap-10 text-white">
-                  <img src="/images/wishlist.svg" alt="wishlist"></img>
-                    <p>Favorite <br/> Wishlist</p>
+                    <img src="/images/wishlist.svg" alt="wishlist"></img>
+                    <p className="mb-0">
+                      Favorite <br /> Wishlist
+                    </p>
                   </Link>
                 </div>
                 <div>
                   <Link className="d-flex align-items-center gap-10 text-white">
-                  <img src="/images/user.svg" alt="user"></img>
-                    <p>Login <br/> My Account</p>
+                    <img src="/images/user.svg" alt="user"></img>
+                    <p className="mb-0">
+                      Login <br /> My Account
+                    </p>
                   </Link>
                 </div>
                 <div>
                   <Link className="d-flex align-items-center gap-10 text-white">
-                  <img src="/images/cart.svg" alt="cart"></img>
-                    <div className="d-flex flex-column">
+                    <img src="/images/cart.svg" alt="cart"></img>
+                    <div className="d-flex flex-column gap-10">
                       <span className="badge bg-white text-dark">0</span>
-                      <p>$ 500</p>
+                      <p className="mb-0">$ 500</p>
                     </div>
                   </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div>
+                  <div className="dropdown">
+                    <button
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center"
+                      type="button"
+                      id="dropdownMenuButton1"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img src="../images/menu.svg" alt=""/>
+                      <span className="me-5 d-inline-block">Shop Categories</span>
+                    </button>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenuButton1"
+                    >
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Another action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Something else here
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/">Blogs</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </div>
                 </div>
               </div>
             </div>
